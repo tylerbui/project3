2087ce8fe758c948d3fa678869f21c28d1a229a0
tylerbui <contact.tylernbui@gmail.com>
2024-01-02 13:17:20 -0800

    updated template

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path,include
+from . import views
 
 urlpatterns = [
-    path('', include('main_app.urls'))
+    path('',views.home, name='home'),
 ]
--- project3/settings.py
+++ project3/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import environ
+import os
+
+environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2sm(wfuu0@0)4^^7_@b)*-sl#j_28+r&282hlfi*giujt1i4re'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
