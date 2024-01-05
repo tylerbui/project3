33708562c93d9be3bf07af784a00b6d71bbe5215
tylerbui <contact.tylernbui@gmail.com>
2024-01-05 11:18:32 -0800

    models

--- main_app/models.py
+++ main_app/models.py
@@ -1,26 +1,25 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
     profile_picture = models.URLField()
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_name = models.CharField(max_length=20)
     bio = models.TextField(default="Bio +") 
 
     def __str__(self):
-        return f'{self.user_name} ({self.id})'
+        return f'{self.user} ({self.id})'
     
 
 class Post(models.Model): 
-    profile_picture = models.ForeignKey(Profile, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    user_name = models.CharField(max_length=20)
-    title = models.CharField(max_length=50)
-    content = models.TextField()
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
+    text_content = models.TextField(max_length=250)
+    post_photo = models.URLField()
     date_posted = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.user_name} ({self.id})'
+        return f'{self.profile.user} ({self.id})'
     
 class Comment(models.Model):
     content = models.TextField()
@@ -29,16 +28,7 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.user_name} ({self.id})'
-
-
-class Photo(models.Model):
-    url = models.CharField(max_length=200)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.name} ({self.id})'
-    
+        return f'{self.user} ({self.id})'
 
 
 
