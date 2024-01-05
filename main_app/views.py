d4c32d09590ce192bb0bd782ff3121cc40514d90
ianm <idmasse@icloud.com>
2024-01-05 11:18:59 -0800

    updates model imports

--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Post, Comment, Profile, Photo
+from .models import Profile, Post, Comment
 
 def home(request):
     posts = Post.objects.all()
