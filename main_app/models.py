e87aa645697d2a9ca73df4eeeb0ec0b42d19a768
tylerbui <contact.tylernbui@gmail.com>
2024-01-04 16:31:45 -0800

    update model for pro pic

--- main_app/models.py
+++ main_app/models.py
@@ -1,22 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-class Post(models.Model): 
+class Profile(models.Model):
     profile_picture = models.URLField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     user_name = models.CharField(max_length=20)
+    bio = models.TextField(default="Bio +") 
+
+    def __str__(self):
+        return f'{self.user_name} ({self.id})'
+    
+
+class Post(models.Model): 
+    profile_picture = models.ForeignKey(Profile, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_name = models.CharField(max_length=20)
     title = models.CharField(max_length=50)
     content = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
 
-    # For Ian to Finish AWS and Boto
-    # class Photo(models.Model):
-    #     url = models.CharField(max_length=200)
-    #     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-
     def __str__(self):
-        return f'{self.name} ({self.id})'
+        return f'{self.user_name} ({self.id})'
     
 class Comment(models.Model):
     content = models.TextField()
@@ -26,15 +30,6 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'{self.user_name} ({self.id})'
-
-class Profile(models.Model):
-    name = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_name = models.CharField(max_length=20)
-    profile_picture = models.URLField()
-    bio = models.TextField(default="Bio +") 
-
-    def __str__(self):
-        return f'{self.name} ({self.user_name})'
 
 
 class Photo(models.Model):
