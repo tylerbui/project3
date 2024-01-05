d501af00c7e104a2efee9c48bcc92806633a2cc6
ianm <idmasse@icloud.com>
2024-01-05 11:14:47 -0800

    Updates models

--- main_app/models.py
+++ main_app/models.py
@@ -2,40 +2,28 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    profile_picture = models.URLField()
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_name = models.CharField(max_length=20)
+    profile_picture = models.URLField()
     bio = models.TextField(default="Bio +") 
 
     def __str__(self):
-        return f'{self.user_name} ({self.id})'
-    
+        return f'{self.user} ({self.id})'
 
 class Post(models.Model): 
-    profile_picture = models.ForeignKey(Profile, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    user_name = models.CharField(max_length=20)
-    title = models.CharField(max_length=50)
-    content = models.TextField()
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
+    text_content = models.TextField(max_length=250)
+    post_photo = models.URLField()
     date_posted = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.user_name} ({self.id})'
-    
+        return f'{self.profile.user.username} ({self.id})'
+
 class Comment(models.Model):
-    content = models.TextField()
-    image = models.URLField()
-    post = models.ManyToManyField(Post)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    comment_text = models.TextField(max_length=150)
 
     def __str__(self):
-        return f'{self.user_name} ({self.id})'
-
-
-class Photo(models.Model):
-    url = models.CharField(max_length=200)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.name} ({self.id})'
+        return f'Comment by {self.user.username} on {self.post} ({self.id})'
     
