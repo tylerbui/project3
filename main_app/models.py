aa9ff3cd74139d3c393d60790043f68341a68c4c
ianm <idmasse@icloud.com>
2024-01-06 16:03:38 -0800

    adds get_absolute_url for profile

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,6 +9,9 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user} ({self.id})'
+    
+    def get_absolute_url(self):
+        return reverse('profile', kwargs={'pk': self.pk})
 
 class Post(models.Model): 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
